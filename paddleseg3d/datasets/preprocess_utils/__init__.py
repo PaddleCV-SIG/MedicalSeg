15755e5325636e0f86f926f0bef4dac61d32d291
langdu <1207540056@qq.com>
2022-04-12 20:40:33 +0800

    update dataset analyzer

--- paddleseg3d/datasets/preprocess_utils/__init__.py
+++ paddleseg3d/datasets/preprocess_utils/__init__.py
@@ -3,3 +3,4 @@ from .geometry import *
 from .uncompress import uncompressor
 from .integrity_checks import verify_dataset_integrity
 from .image_crop import crop
+from .dataset_analyzer import DatasetAnalyzer
