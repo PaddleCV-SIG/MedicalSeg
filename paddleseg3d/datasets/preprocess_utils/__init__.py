19d1d603818e90dc4a491b5a469a3a4dfcad50cc
shiyutang <34859558+shiyutang@users.noreply.github.com>
2022-06-13 16:40:37 +0800

    Revert "[Feature] Add Experiment plan and Preocess"

--- paddleseg3d/datasets/preprocess_utils/__init__.py
+++ paddleseg3d/datasets/preprocess_utils/__init__.py
@@ -5,6 +5,3 @@ from .convert_to_decathlon import convert_to_decathlon
 from .integrity_checks import verify_dataset_integrity
 from .image_crop import crop
 from .dataset_analyzer import DatasetAnalyzer
-from .preprocessing import GenericPreprocessor, PreprocessorFor2D
-from .experiment_utils import *
-from .experiment_planner import ExperimentPlanner2D_v21, ExperimentPlanner3D_v21
